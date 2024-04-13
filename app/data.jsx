940e79c607987bb886ef36fd7247d67878699f20
DArzoEsp <davidarzola80@gmail.com>
2024-04-13 14:14:25 -0700

    updated pictures

--- app/data.jsx
+++ app/data.jsx
@@ -3,28 +3,28 @@ export default [
         id: 1,
         projectName: 'EasyBank Landing Page' ,
         projectDescription: 'Front-End project. It is an example of a bank landing page. Used mostly Sass with combination of HTML and Javascript',
-        projectImg: 'easy-bank-frontend.png',
+        projectImg: '../img/easy-bank-frontend.png',
         projectLink: 'https://easy-bank-frontend.netlify.app/'
     },
     {
         id: 2,
         projectName: 'Recreate Pokemon',
         projectDescription: 'A game like pokemon. Used mostly vanilla Javascript for logic, Tiled to make the map, and GSAP to animate this game. Learned a lot about 2D game development',
-        projectImg: 'pokemon-screenshot.png',
+        projectImg: '../img/pokemon-screenshot.png',
         projectLink: 'https://pokemon-recreate.netlify.app/'
     },
     {
         id: 3,
         projectName: 'Travel Journal',
         projectDescription: 'Example of Travel Journal. Used React, CSS, and JS to style and create it. Has scrollable features and a max width and height',
-        projectImg: 'travel-journal.png',
+        projectImg: '../img/travel-journal.png',
         projectLink: 'https://travel-journal-example.netlify.app/'
     },
     {
         id: 4,
         projectName: 'Frogger Recreate',
         projectDescription: 'Frogger game. Made mostly by JS and HTML. It is a very simple game but functional.',
-        projectImg: '/frogger.png',
+        projectImg: '../img/frogger.png',
         projectLink: 'https://frogger-recreate.netlify.app'
     },
     // {
