cd6f0788504a6c8e68ddd57d9b5b7d26eba79e9f
DArzoEsp <davidarzola80@gmail.com>
2024-04-13 17:32:58 -0700

    updated

--- app/data.jsx
+++ app/data.jsx
@@ -2,28 +2,28 @@ export default [
     {
         id: 1,
         projectName: 'EasyBank Landing Page' ,
-        projectDescription: 'Front-End project. It is an example of a bank landing page. Used mostly Sass with combination of HTML and Javascript',
+        projectDescription: 'Front-End project. It is an example of a bank landing page. Made with mostly Sass with combination of HTML and Javascript',
         projectImg: '/images/easy-bank-frontend.png',
         projectLink: 'https://easy-bank-frontend.netlify.app/'
     },
     {
         id: 2,
         projectName: 'Recreate Pokemon',
-        projectDescription: 'A game like pokemon. Used mostly vanilla Javascript for logic, Tiled to make the map, and GSAP to animate this game. Learned a lot about 2D game development',
+        projectDescription: 'A game like pokemon. Made using vanilla Javascript for logic, Tiled to make the map, and GSAP to animate this game. Learned a lot about 2D game development',
         projectImg: '/images/pokemon-screenshot.png',
         projectLink: 'https://pokemon-recreate.netlify.app/'
     },
     {
         id: 3,
         projectName: 'Travel Journal',
-        projectDescription: 'Example of Travel Journal. Used React, CSS, and JS to style and create it. Has scrollable features and a max width and height',
+        projectDescription: 'Example of Travel Journal. Made with React, CSS, and JS to style and create it. Has scrollable features and a max width and height',
         projectImg: '/images/travel-journal.png',
         projectLink: 'https://travel-journal-example.netlify.app/'
     },
     {
         id: 4,
         projectName: 'Frogger Recreate',
-        projectDescription: 'Frogger game. Made mostly by JS and HTML. It is a very simple game but functional.',
+        projectDescription: 'Frogger game. Made with Javascript and HTML. It is a very simple game but functional.',
         projectImg: '/images/frogger.png',
         projectLink: 'https://frogger-recreate.netlify.app'
     },
